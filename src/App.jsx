15bd8d72ecc7fa88f8842fb2f69d847796e2d768
mohamedmehdigara <garamehdi57@yahoo.com>
2022-12-16 10:44:48 +0100

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import Contact from "./Contact";
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 function App() {
-  return (
-    <div className="App">
-      <Contact />
-    </div>
-  );
-}
+  const [contacts, setContacts] = useState([]);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    axios("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        setContacts(response.data);
+        setError(null);
+      })
+      .catch(setError);}, []);
+      if (error) return <p>An error occurred</p>
 
-export default App;
+      return (
+        <div className="App">
+          {contacts.map(({ id, name, email, company }) => (
+            <Contact
+              key={id}
+              name={name}
+              email={email}
+              tagline={company.catchPhrase}
+            />
+          ))}
+        </div>
+      );
+    }
+    
+    export default App;
